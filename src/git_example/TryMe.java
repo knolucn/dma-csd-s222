5ef420810c2e47e77385ca329925c018b9245174
knolucn <knol@ucn.dk>
2022-11-06 20:38:36 +0100

    attempted conflict

--- src/git_example/TryMe.java
+++ src/git_example/TryMe.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class TryMe {
 	public static void main(String[] args) {
+		System.out.println("knol");
 		System.out.print("Say a word: ");
 		Scanner scanner = new Scanner(System.in);
 		String word = scanner.nextLine();
-		System.out.println("Hello, " + word + "!");
+		System.out.println("Hello, " + word + "!"); 
 		
 	}
 }
